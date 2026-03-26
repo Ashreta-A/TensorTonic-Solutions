351994e24d43c723605ca3aa62980b85d495b4f2
Ashreta_A <ashretareddy3010@gmail.com>
2026-03-26 14:10:54 +0530

    Add solution for dot-product

---
+++ dot-product/dot-product.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def dot_product(x, y):
+    """
+    Compute the dot product of two 1D arrays x and y.
+    Must return a float.
+    """
+    z = np.dot(x, y);
+    return z;
+    # Write code here
+    pass
